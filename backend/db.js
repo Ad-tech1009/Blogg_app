b2621cf88258b812e20bbee53581f0582bef3a3e
aditya <adityaaryan1009@gmail.com>
2024-10-27 09:53:38 +0530

    All files updated

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
-
-const dataBase=mongoose.connect(process.env.MONGO_URL).then(()=>{
-    console.log("Database is connected successfully !")
-}).catch((err)=>{
-    console.log(`error is ${err}`)
-})
-
